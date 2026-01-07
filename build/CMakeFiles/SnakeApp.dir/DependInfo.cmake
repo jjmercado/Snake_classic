27540f5772d2510c00d1984fc7fc6286eb313d62
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2026-01-07 20:29:32 +0100

    add: cmake to generate executables for different plattforms

---
+++ build/CMakeFiles/SnakeApp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/Food.cpp" "CMakeFiles/SnakeApp.dir/Snake/Food.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/Food.cpp.o.d"
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/Game.cpp" "CMakeFiles/SnakeApp.dir/Snake/Game.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/Game.cpp.o.d"
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/Grid.cpp" "CMakeFiles/SnakeApp.dir/Snake/Grid.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/Grid.cpp.o.d"
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/Main.cpp" "CMakeFiles/SnakeApp.dir/Snake/Main.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/Main.cpp.o.d"
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/Snake.cpp" "CMakeFiles/SnakeApp.dir/Snake/Snake.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/Snake.cpp.o.d"
+  "/home/jeremy/Development/C++/Classic_Snake/Snake/SnakePart.cpp" "CMakeFiles/SnakeApp.dir/Snake/SnakePart.cpp.o" "gcc" "CMakeFiles/SnakeApp.dir/Snake/SnakePart.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
